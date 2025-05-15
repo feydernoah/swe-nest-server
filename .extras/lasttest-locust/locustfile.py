be02e042803a8ca8685c10655f6381e7a0ecbeca
LukasBalzuweit <lukas.balzuweit@gmail.com>
2025-05-15 14:46:22 +0200

    Lasttests mit Locust

--- .extras/lasttest-locust/locustfile.py
+++ .extras/lasttest-locust/locustfile.py
@@ -13,60 +13,68 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-"""Lasttest mit Locust."""
-
+#Lasttest-Locust
 from typing import Final
 
 import urllib3
 from locust import HttpUser, constant_throughput, task
+import requests
 
-# https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho#answer-44615889
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+KEYCLOAK_URL = "https://localhost:8880/realms/nest/protocol/openid-connect/token"
+KEYCLOAK_CLIENT_ID = "nest-client"
+KEYCLOAK_CLIENT_SECRET = "hFfxabQyuI8XABCM9Rlh69qQpfLFCvoq"  
+KEYCLOAK_USERNAME = "admin"
+KEYCLOAK_PASSWORD = "p"
 
 # https://docs.locust.io/en/stable/api.html#httpuser-class
-class BuchRequests(HttpUser):
-    """Lasttest für HTTP-Requests fuer Server Buch."""
+class BikeRequests(HttpUser):
+
+    host = "https://localhost:3000"
 
     # https://docs.locust.io/en/stable/writing-a-locustfile.html#wait-time-attribute
     # https://docs.locust.io/en/stable/api.html#locust.User.wait_time
     # https://docs.locust.io/en/stable/api.html#locust.wait_time.constant_throughput
-    # 50 "Task Iterations" pro Sekunde
-    wait_time = constant_throughput(0.1)  # type: ignore[no-untyped-call]
+    wait_time = constant_throughput(0.1)  # 100 Requests pro Sekunde pro User
     MIN_USERS: Final = 500
     MAX_USERS: Final = 500
+    token = None
 
     # https://docs.locust.io/en/stable/writing-a-locustfile.html#on-start-and-on-stop-methods
     def on_start(self) -> None:
-        """Initialisierung: selbst-signiertes Zertifikat erlauben."""
         self.client.verify = False
+        url = f"{self.host}/auth/token"
+        headers = {"Content-Type": "application/json"}
+        body = {"username": KEYCLOAK_USERNAME, "password": KEYCLOAK_PASSWORD}
+        response = requests.post(url, headers=headers, json=body, verify=False)
+        response.raise_for_status()
+        self.token = response.json()["access_token"]
 
     # https://docs.locust.io/en/stable/api.html#locust.task
     # https://docs.locust.io/en/stable/api.html#locust.User.weight
+    #GET-Requests mit Pfadvariable.
     @task(100)
     def get_id(self) -> None:
-        """GET-Requests mit Pfadparameter: Buch-ID."""
-        id_list: Final = [1, 20, 30, 40, 50, 60]
-        for buch_id in id_list:
-            self.client.get(f"/rest/{buch_id}")
+        id_list: Final = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        headers = {"Authorization": f"Bearer {self.token}"}
+        for bike_id in id_list:
+            self.client.get(f"/bike/{bike_id}", headers=headers)
 
-    @task(200)
-    def get_titel(self) -> None:
-        """GET-Requests mit Query-Parameter: Teilstring des Buchtitels."""
-        titel_list = ["a", "l", "t", "i", "p"]
-        for teil in titel_list:
-            self.client.get("/rest", params={"titel": teil})
+    #GET-Requests mit Query-Parameter: brand.
+    @task(80)
+    def get_brand(self) -> None:
+        brands = ["Trek", "Giant", "Specialized"]
+        headers = {"Authorization": f"Bearer {self.token}"}
+        for brand in brands:
+            self.client.get("/bike", params={"brand": brand}, headers=headers)
 
-    @task(150)
-    def get_isbn(self) -> None:
-        """GET-Requests mit Query-Parameter: ISBN-Nummer."""
-        isbn_list: Final = [
-            "978-3-897-22583-1",
-            "978-3-827-31552-6",
-            "978-0-201-63361-0",
-            "978-0-007-09732-6",
-            "978-3-824-40481-0",
-            "978-3-540-43081-0",
-        ]
-        for isbn in isbn_list:
-            self.client.get("/rest", params={"isbn": isbn})
+    #GET-Requests mit Query-Parameter: type.
+    @task(60)
+    def get_type(self) -> None:
+        types = ["Mountain", "Road"]
+        headers = {"Authorization": f"Bearer {self.token}"}
+        for t in types:
+            self.client.get("/bike", params={"type": t}, headers=headers)
+
+
--- src/bike/controller/bike-get.controller.ts
+++ src/bike/controller/bike-get.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { BikeReadService } from '../service/bike-read.service.js';
 
 @Controller() // Basis-Route ist leer, damit beide Endpunkte unabhängig sind
@@ -13,6 +13,13 @@ export class BikeGetController {
   async findAll(): Promise<string> {
     const bikes = await this.bikeReadService.findAll();
     return JSON.stringify(bikes);
+  }
+
+  @Get('bike/:id') // Endpunkt: /bike/:id
+  async findOne(@Param('id') id: string): Promise<string> {
+    const bikes = await this.bikeReadService.findAll();
+    const bike = bikes.find(b => String(b.id) === id);
+    return bike ? JSON.stringify(bike) : JSON.stringify({ error: 'Bike not found' });
   }
 
   @Get('bikewithtitles') // Endpunkt: /bikewithtitles
